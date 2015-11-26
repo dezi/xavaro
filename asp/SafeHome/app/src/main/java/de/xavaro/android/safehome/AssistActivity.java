708320c3cf07f278e3eb64cb18c222c137228398
dezi <dezi@kappa-mm.de>
2015-11-26 10:43:30 +0100

    Added assist activity.

---
+++ asp/SafeHome/app/src/main/java/de/xavaro/android/safehome/AssistActivity.java
@@ -0,0 +1,15 @@
+package de.xavaro.android.safehome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssistActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assist);
+    }
+}
