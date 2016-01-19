46bbfc49b14bf7059a8b013ff7d1e7a5ffd7210b
dezi <dezi@kappa-mm.de>
2016-01-19 17:56:22 +0100

    Check in.

---
+++ php/epgdaemon/GetAstraChannels.php
@@ -0,0 +1,7 @@
+<?php
+
+include("../php/json.php")
+
+// http://www.astra.de/webservice/v3/channels?limit=100&page=23&sort_by=name&sort_direction=asc&domain_identifier=d3d3LmFzdHJhLmRl&free=yes&pay=yes&mode=tv
+
+?>
