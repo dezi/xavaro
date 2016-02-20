e0ee37eb8de08d41aa8c4cc245d886c73a144ae1
patrickzyolo <patrick@kappa-mm.de>
2016-02-20 15:53:11 +0100

    check in.

--- asp/SafeHome/app/webapps/tvguide/main.js
+++ asp/SafeHome/app/webapps/tvguide/main.js
@@ -2,4 +2,4 @@ console.log("hasenpups");
 
 //alert("da bin ich...");
 
-document.body.innerHTML = '<a href="http://m.spiegel.de/"> nix mehr dran####????</a>';
+document.body.innerHTML = '<a href="http://m.spiegel.de/"> nix mehr dran!!!!!!</a>';
