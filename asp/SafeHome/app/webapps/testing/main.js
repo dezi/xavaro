b9a24dc72d9b63bcd51d61cae4407a09bdbd93d2
Dennis Zierahn <dezi@kappa-mm.de>
2016-02-21 15:00:02 +0100

    Check in.

--- asp/SafeHome/app/webapps/testing/main.js
+++ asp/SafeHome/app/webapps/testing/main.js
@@ -3,5 +3,12 @@ console.log("==================main.js loaded...");
 //testing.alertest();
 
 console.log("==================" + extras.loadSync("http://epg.xavaro.de/channels/tv/de/ZDF.json"));
-console.log("==================" + extras.loadSync("http://epg.xavaro.de/channels/tv/de.json.gz"));
 
+testing.channels = extras.loadSync("http://epg.xavaro.de/channels/tv/de.json.gz");
+testing.ZDF = extras.loadSync("http://epg.xavaro.de/epgdata/tv/de/ZDF/current.json.gz");
+
+console.log("==================" + testing.channels.length);
+
+var pre = document.createElement("pre");
+document.body.appendChild(pre);
+pre.innerHTML = testing.channels + testing.ZDF;
