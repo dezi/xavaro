dcd89b91201b2e30f428b33d5e6c6d3d695a06d9
patrickzyolo <patrick@kappa-mm.de>
2016-02-23 22:02:07 +0100

    Clean up.

--- asp/SafeHome/app/webapps/tvguide/main.js
+++ asp/SafeHome/app/webapps/tvguide/main.js
@@ -17,44 +17,43 @@ tvguide.getSenderList = function()
     //
     //
 
-    var obj = JSON.parse(WebAppRequest.loadSync("http://epg.xavaro.de/channels/tv/de.json.gz"));
+//    var obj = JSON.parse(extras.loadSync("http://epg.xavaro.de/channels/tv/de.json.gz"));
+//
+//    tvguide.senderList = [];
+//
+//    for(var index in obj)
+//    {
+//        tvguide.senderList.push("tv/de/" + obj[ index ].name);
+//    }
+//
+//    console.log(tvguide.getSenderList);
 
-    tvguide.senderList = [];
-
-    for(var index in obj)
-    {
-        tvguide.senderList
-        .push("tv/de/" + obj[ index ].name);
-    }
-
-    console.log(tvguide.getSenderList);
-
-//    tvguide.senderList =
-//    [
-//        "tv/de/ZDF",
-//        "tv/de/Folx",
-//        "tv/de/N24",
-//        "tv/de/N-TV",
-//        "tv/de/NDR Fernsehen Hamburg",
-//        "tv/de/Phoenix",
-//        "tv/de/TV Allgäu",
-//        "tv/de/Sport 1",
-//        "tv/de/Tele 5",
-//        "tv/de/WDR Studio Dortmund",
-//        "tv/de/Sky Select 9",
-//        "tv/de/Sky Nostalgie",
-//        "tv/de/RTL Passion",
-//        "tv/de/Romance TV Deutschland",
-//        "tv/de/QVC Deutschland",
-//        "tv/de/Pearl TV",
-//        "tv/de/NDR Fernsehen Hamburg",
-//        "tv/de/Nickelodeon Deutschland HD",
-//        "tv/de/MTV Germany HD",
-//        "tv/de/MDR Fernsehen Thüringen HD",
-//        "tv/de/Kinowelt TV",
-//        "tv/de/Juwelo",
-//        "tv/de/Das Erste"
-//    ];
+    tvguide.senderList =
+    [
+        "tv/de/ZDF",
+        "tv/de/Folx",
+        "tv/de/N24",
+        "tv/de/N-TV",
+        "tv/de/NDR Fernsehen Hamburg",
+        "tv/de/Phoenix",
+        "tv/de/TV Allgäu",
+        "tv/de/Sport 1",
+        "tv/de/Tele 5",
+        "tv/de/WDR Studio Dortmund",
+        "tv/de/Sky Select 9",
+        "tv/de/Sky Nostalgie",
+        "tv/de/RTL Passion",
+        "tv/de/Romance TV Deutschland",
+        "tv/de/QVC Deutschland",
+        "tv/de/Pearl TV",
+        "tv/de/NDR Fernsehen Hamburg",
+        "tv/de/Nickelodeon Deutschland HD",
+        "tv/de/MTV Germany HD",
+        "tv/de/MDR Fernsehen Thüringen HD",
+        "tv/de/Kinowelt TV",
+        "tv/de/Juwelo",
+        "tv/de/Das Erste"
+    ];
 }
 
 tvguide.onOrientationChange = function()
@@ -71,17 +70,14 @@ tvguide.onTouchStart = function(event)
 
     tvguide.touch = {};
 
-    target =  touchobj.target;
+    target = touchobj.target;
 
     while (target && target.id != "senderbarScroll")
     {
         target = target.parentElement;
     }
 
-    if (!target) return // war nix
-
-// =====> dieses element muss gescrollt werden.
-//
+    if (!target) return;
 
     tvguide.touch.startX = touchobj.clientX;
     tvguide.touch.startY = touchobj.clientY;
@@ -98,11 +94,7 @@ tvguide.onTouchMove = function(event)
 {
     var touchobj = event.changedTouches[ 0 ];
 
-    //console.log("onTouchMove:" + touchobj.clientX + "/" + touchobj.clientY);
-
-    // hier senderbarScroll am offsetTop rumdüdeln.-...
-//    senderbarScroll.offsetTop = tvguide.touch.startY - touchobj.clientY;
-    deltay = touchobj.clientY - tvguide.touch.startY;
+    var deltay = touchobj.clientY - tvguide.touch.startY;
     tvguide.touch.target.style.top = (tvguide.touch.offsetTop + deltay) + "px";
 
     event.preventDefault();
@@ -127,129 +119,156 @@ tvguide.createFrameSetup = function()
     document.body.style.margin = '0px';
     document.body.style.padding = '0px';
 
-    var div = document.createElement("div");
-    div.id = "topdiv";
-    div.style.position = "absolute";
-    div.style.top = "0px";
-    div.style.left = "0px";
-    div.style.bottom = "0px";
-    div.style.right = "0px";
+    //
+    // topdiv
+    //
+
+    var div                   = document.createElement("div");
+    div.id                    = "topdiv";
+    div.style.position        = "absolute";
+    div.style.top             = "0px";
+    div.style.left            = "0px";
+    div.style.right           = "0px";
+    div.style.bottom          = "0px";
     div.style.backgroundColor = "#80ff00";
+    tvguide.topdiv            = div;
+
     document.body.appendChild(div);
 
-    tvguide.topdiv = div;
+    //
+    // topdiv.titlebar
+    //
 
-    var div = document.createElement("div");
-    div.id = "titlebar";
-    div.style.position = "absolute";
-    div.style.top = "0px";
-    div.style.left = "0px";
-    div.style.height = tvguide.constans.titlebarHeight + "px";
-    div.style.right = "0px";
+    var div                   = document.createElement("div");
+    div.id                    = "titlebar";
+    div.style.position        = "absolute";
+    div.style.top             = "0px";
+    div.style.left            = "0px";
+    div.style.right           = "0px";
+    div.style.height          = tvguide.constans.titlebarHeight + "px";
     div.style.backgroundColor = "#8080ff";
+    tvguide.titlebar          = div;
+
     tvguide.topdiv.appendChild(div);
 
-    tvguide.titlebar = div;
+    //
+    // topdiv.content1
+    //
 
-    var div = document.createElement("div");
-    div.id = "content1";
-    div.style.position = "absolute";
-    div.style.top = tvguide.constans.titlebarHeight + "px";
-    div.style.left = "0px";
-    div.style.bottom = "0px";
-    div.style.right = "0px";
+    var div                   = document.createElement("div");
+    div.id                    = "content1";
+    div.style.position        = "absolute";
+    div.style.top             = tvguide.constans.titlebarHeight + "px";
+    div.style.left            = "0px";
+    div.style.right           = "0px";
+    div.style.bottom          = "0px";
     div.style.backgroundColor = "#ff8000";
+    tvguide.content1          = div;
+
     tvguide.topdiv.appendChild(div);
 
-    tvguide.content1 = div;
+    //
+    // topdiv.content1.senderbar
+    //
 
-    var div = document.createElement("div");
-    div.id = "senderbar";
-    div.style.position = "absolute";
-    div.style.top = "0px";
-    div.style.left = "0px";
-    div.style.bottom = "0px";
-    div.style.width = tvguide.constans.senderbarWidth + "px";
+    var div                   = document.createElement("div");
+    div.id                    = "senderbar";
+    div.style.position        = "absolute";
+    div.style.top             = "0px";
+    div.style.left            = "0px";
+    div.style.bottom          = "0px";
+    div.style.width           = tvguide.constans.senderbarWidth + "px";
     div.style.backgroundColor = "#000000";
-    div.style.overflow = "hidden";
+    div.style.overflow        = "hidden";
+    tvguide.senderbar         = div;
 
     tvguide.content1.appendChild(div);
 
-    tvguide.senderbar = div;
+    //
+    // topdiv.content1.content2
+    //
 
-    var div = document.createElement("div");
-    div.id = "content2";
-    div.style.position = "absolute";
-    div.style.top = "0px";
-    div.style.left = tvguide.constans.senderbarWidth + "px";
-    div.style.bottom = "0px";
-    div.style.right = "0px";
+    var div                   = document.createElement("div");
+    div.id                    = "content2";
+    div.style.position        = "absolute";
+    div.style.top             = "0px";
+    div.style.left            = tvguide.constans.senderbarWidth + "px";
+    div.style.right           = "0px";
+    div.style.bottom          = "0px";
     div.style.backgroundColor = "#ffff00";
+    tvguide.content2          = div;
+
     tvguide.content1.appendChild(div);
 
-    tvguide.content2 = div;
+    //
+    // topdiv.content1.content2.timeline
+    //
 
-    var div = document.createElement("div");
-    div.id = "timeline";
-    div.style.position = "absolute";
-    div.style.top = "0px";
-    div.style.left = "0px";
-    div.style.height = tvguide.constans.timelineHeight + "px";
-    div.style.right = "0px";
+    var div                   = document.createElement("div");
+    div.id                    = "timeline";
+    div.style.position        = "absolute";
+    div.style.top             = "0px";
+    div.style.left            = "0px";
+    div.style.right           = "0px";
+    div.style.height          = tvguide.constans.timelineHeight + "px";
     div.style.backgroundColor = "#ff00ff";
+    tvguide.timeline          = div;
+
     tvguide.content2.appendChild(div);
 
-    tvguide.timeline = div;
+    //
+    // topdiv.content1.content2.epgdata
+    //
 
-    var div = document.createElement("div");
-    div.id = "epgdata";
-    div.style.position = "absolute";
-    div.style.top = tvguide.constans.timelineHeight + "px";
-    div.style.left = "0px";
-    div.style.bottom = "0px";
-    div.style.right = "0px";
+    var div                   = document.createElement("div");
+    div.id                    = "epgdata";
+    div.style.position        = "absolute";
+    div.style.top             = tvguide.constans.timelineHeight + "px";
+    div.style.left            = "0px";
+    div.style.right           = "0px";
+    div.style.bottom          = "0px";
     div.style.backgroundColor = "#ffffff";
-    tvguide.content2.appendChild(div);
+    tvguide.epgdata           = div;
 
-    tvguide.epgdata = div;
+    tvguide.content2.appendChild(div);
 }
 
 tvguide.createSenderBar = function()
 {
-    var div = document.createElement("div");
-    div.id = "senderbarScroll";
+    var div            = document.createElement("div");
+    div.id             = "senderbarScroll";
     div.style.position = "absolute";
-    div.style.top   = "0px";
-    div.style.left  = "0px";
-    div.style.right = "0px";
-
-    tvguide.senderbar.appendChild(div);
+    div.style.top      = "0px";
+    div.style.left     = "0px";
+    div.style.right    = "0px";
 
     tvguide.senderbarScroll = div;
+    tvguide.senderbar.appendChild(div);
 
     for(var index in tvguide.senderList)
     {
-        var div = document.createElement("div");
-        div.style.position = "absolute";
-        div.style.top      = tvguide.constans.timelineHeight + tvguide.constans.senderHeight * index + "px";
-        div.style.height   = tvguide.constans.senderHeight + "px";
-        div.style.width    = tvguide.constans.senderbarWidth + "px";
-        div.style.left     = "0px";
-        div.style.right    = "0px";
+        var senderDiv            = document.createElement("div");
+        senderDiv.style.position = "absolute";
+        senderDiv.style.top      = tvguide.constans.timelineHeight +
+                                   tvguide.constans.senderHeight * index + "px";
+        senderDiv.style.height   = tvguide.constans.senderHeight + "px";
+        senderDiv.style.width    = tvguide.constans.senderbarWidth + "px";
+        senderDiv.style.left     = "0px";
+        senderDiv.style.right    = "0px";
 
-        var divPadding = document.createElement("div");
-//        divPadding.style.position = "absolute";
-        divPadding.style.padding = "5px";
-        divPadding.style.backgroundColor = "#ed1a79";
+        var paddingDiv                   = document.createElement("div");
+        paddingDiv.align                 = "center";
+        paddingDiv.style.padding         = "5px";
+        paddingDiv.style.backgroundColor = "#ed1a79";
 
         var image = document.createElement("img");
 
-        image.src = encodeURI(
-            "http://" + tvguide.manifest.appserver +
-            "/channels/" + tvguide.senderList[ index ] + ".png"
+        image.src = encodeURI("http://" +
+            tvguide.manifest.appserver + "/channels/" +
+            tvguide.senderList[ index ] + ".png"
         );
 
-        image.id = tvguide.senderList[ index ];
+        image.id           = tvguide.senderList[ index ];
         image.style.top    = "0px";
         image.style.left   = "0px";
         image.style.right  = "0px";
@@ -257,11 +276,10 @@ tvguide.createSenderBar = function()
         image.style.height = tvguide.constans.senderHeight + "px";
 
 
-        divPadding.appendChild(image)
-        div.appendChild(divPadding)
+        paddingDiv.appendChild(image)
+        senderDiv.appendChild(paddingDiv)
 
-        // tvguide.topdiv = div;
-        tvguide.senderbarScroll.appendChild(div);
+        tvguide.senderbarScroll.appendChild(senderDiv);
     }
 
     senderbarScroll.clientHeight = tvguide.senderList.length * tvguide.constans.senderHeight;
