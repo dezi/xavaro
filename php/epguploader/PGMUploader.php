81b06aa8f71fd8b6e7284f2079bb7d86cb94b2e2
Kappa Motion Media <kappa@Debian-60-squeeze-64-LAMP.(none)>
2016-02-25 17:58:14 +0100

    Check in.

--- php/epguploader/PGMUploader.php
+++ php/epguploader/PGMUploader.php
@@ -48,39 +48,56 @@
 	 	@mkdir($lpath, 0755, true);
 	 	$lpath .= "/" . $name;
 		
-		$jpeg = file_get_contents($imgurl);
-		if ($jpeg == false) exit();
-		
-		//
-		// Write original.
-		//
-		
 		$orig = $lpath . ".orig.jpg";
 		$thmb = $lpath . ".thmb.jpg";
 		
-		file_put_contents($orig, $jpeg);
-		chmod($orig, 0644);
-		error_log("PGMUploader: PUT $orig");
+		$jpeg = file_get_contents($imgurl);
+		
+		if ($jpeg != false)
+		{
+			//
+			// Write original.
+			//
+		
+			file_put_contents($orig, $jpeg);
+			chmod($orig, 0644);
+			error_log("PGMUploader: PUT $orig");
 	 
-		//
-		// Write thumbnail.
-		//
+			//
+			// Write thumbnail.
+			//
 
-		$source_image = imagecreatefromjpeg($orig);
-		$width = imagesx($source_image);
-		$height = imagesy($source_image);
-	
-		$desired_width = 256;
-		$desired_height = floor($height * ($desired_width / $width));
-		
-		$virtual_image = imagecreatetruecolor($desired_width, $desired_height);
-		
-		imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, 
-			$desired_width, $desired_height, $width, $height);
+			$source_image = @imagecreatefromjpeg($orig);
 			
-		imagejpeg($virtual_image, $thmb);    
-		chmod($thmb, 0644);
+			if ($source_image != false)
+			{
+				$width = imagesx($source_image);
+				$height = imagesy($source_image);
+	
+				$desired_width = 256;
+				$desired_height = floor($height * ($desired_width / $width));
 		
-		error_log("PGMUploader: PUT $thmb");
+				$virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+		
+				imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, 
+					$desired_width, $desired_height, $width, $height);
+			
+				imagejpeg($virtual_image, $thmb);    
+				chmod($thmb, 0644);
+		
+				error_log("PGMUploader: PUT $thmb");
+			}
+		}
+		
+		if (! file_exists($thmb)) 
+		{
+			@unlink($orig);
+			
+			header('HTTP/1.0 404 Not found');
+		}
+		else
+		{
+			header('HTTP/1.0 200 Ok');
+		}
 	}
 ?>
