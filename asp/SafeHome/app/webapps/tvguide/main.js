8d6a7b1c9c755bf5a5b1320e3b56131ab9a3c694
patrickzyolo <patrick@kappa-mm.de>
2016-03-07 17:07:19 +0100

    check in.

--- asp/SafeHome/app/webapps/tvguide/main.js
+++ asp/SafeHome/app/webapps/tvguide/main.js
@@ -1,9 +1,9 @@
-tvguide.constans =
+tvguide.constanst =
 {
     titlebarHeight : 80,
     senderbarWidth : 80,
-    timelineHeight : 20,
-    senderHeight   : 50
+    senderHeight   : 64,
+    timelineHeight : 60
 }
 
 tvguide.getSenderList = function()
@@ -61,239 +61,124 @@ tvguide.onOrientationChange = function()
     console.log(window.orientation);
 }
 
-tvguide.onTouchStart = function(event)
-{
-    var touchobj = event.changedTouches[ 0 ];
-
-    console.log("onTouchStart:" + touchobj.target.id + "=" + touchobj.clientX + "/" + touchobj.clientY);
-    console.log("onTouchStart:" + touchobj.target.innerHTML);
-
-    tvguide.touch = {};
-
-    target = touchobj.target;
-
-    while (target && target.id != "senderbarScroll")
-    {
-        target = target.parentElement;
-    }
-
-    if (!target) return;
-
-    tvguide.touch.startX = touchobj.clientX;
-    tvguide.touch.startY = touchobj.clientY;
-
-    tvguide.touch.offsetTop  = target.offsetTop;
-    tvguide.touch.offsetLeft = target.offsetLeft;
-
-    tvguide.touch.target = target;
-
-    event.preventDefault();
-}
-
-tvguide.onTouchMove = function(event)
-{
-    var touchobj = event.changedTouches[ 0 ];
-
-    var deltay = touchobj.clientY - tvguide.touch.startY;
-    tvguide.touch.target.style.top = (tvguide.touch.offsetTop + deltay) + "px";
-
-    event.preventDefault();
-}
-
-tvguide.onTouchEnd = function(event)
-{
-    var touchobj = event.changedTouches[ 0 ];
-
-    console.log("onTouchEnd:" + touchobj.clientX + "/" + touchobj.clientY);
-
-    event.preventDefault();
-}
-
-window.addEventListener("orientationchange", tvguide.onOrientationChange);
-window.addEventListener("touchstart", tvguide.onTouchStart);
-window.addEventListener("touchmove", tvguide.onTouchMove);
-window.addEventListener("touchend", tvguide.onTouchEnd);
-
 tvguide.createFrameSetup = function()
 {
-    document.body.style.margin = '0px';
-    document.body.style.padding = '0px';
-
     //
     // topdiv
     //
 
-    var div                   = document.createElement("div");
-    div.id                    = "topdiv";
-    div.style.position        = "absolute";
-    div.style.top             = "0px";
-    div.style.left            = "0px";
-    div.style.right           = "0px";
-    div.style.bottom          = "0px";
-    div.style.backgroundColor = "#80ff00";
-    tvguide.topdiv            = div;
-
-    document.body.appendChild(div);
+    tvguide.topdiv = WebLibSimple.createDiv(0, 0, 0, 0, "topdiv", document.body);
 
     //
     // topdiv.titlebar
     //
 
-    var div                   = document.createElement("div");
-    div.id                    = "titlebar";
-    div.style.position        = "absolute";
-    div.style.top             = "0px";
-    div.style.left            = "0px";
-    div.style.right           = "0px";
-    div.style.height          = tvguide.constans.titlebarHeight + "px";
-    div.style.backgroundColor = "#8080ff";
-    tvguide.titlebar          = div;
-
-    tvguide.topdiv.appendChild(div);
+    tvguide.titlebar = WebLibSimple.createDivHeight(0, 0, 0, tvguide.constanst.titlebarHeight, "titlebar", tvguide.topdiv);
+    WebLibSimple.setBGColor(tvguide.titlebar, "#8080ff");
 
     //
     // topdiv.content1
     //
 
-    var div                   = document.createElement("div");
-    div.id                    = "content1";
-    div.style.position        = "absolute";
-    div.style.top             = tvguide.constans.titlebarHeight + "px";
-    div.style.left            = "0px";
-    div.style.right           = "0px";
-    div.style.bottom          = "0px";
-    div.style.backgroundColor = "#ff8000";
-    tvguide.content1          = div;
-
-    tvguide.topdiv.appendChild(div);
+    tvguide.content1 = WebLibSimple.createDiv(0, tvguide.constanst.titlebarHeight, 0, 0, "content1", tvguide.topdiv);
+    WebLibSimple.setBGColor(tvguide.content1, "#ff8000");
 
     //
     // topdiv.content1.senderbar
     //
 
-    var div                   = document.createElement("div");
-    div.id                    = "senderbar";
-    div.style.position        = "absolute";
-    div.style.top             = "0px";
-    div.style.left            = "0px";
-    div.style.bottom          = "0px";
-    div.style.width           = tvguide.constans.senderbarWidth + "px";
-    div.style.backgroundColor = "#000000";
-    div.style.overflow        = "hidden";
-    tvguide.senderbar         = div;
+    tvguide.senderbar = WebLibSimple.createDivWidth(0, 0, tvguide.constanst.senderbarWidth, 0, "senderbar", tvguide.content1);
+    tvguide.senderbar.style.overflow = "hidden";
 
-    tvguide.content1.appendChild(div);
+    WebLibSimple.setBGColor(tvguide.senderbar, "#000000");
+
 
     //
     // topdiv.content1.content2
     //
 
-    var div                   = document.createElement("div");
-    div.id                    = "content2";
-    div.style.position        = "absolute";
-    div.style.top             = "0px";
-    div.style.left            = tvguide.constans.senderbarWidth + "px";
-    div.style.right           = "0px";
-    div.style.bottom          = "0px";
-    div.style.backgroundColor = "#ffff00";
-    tvguide.content2          = div;
-
-    tvguide.content1.appendChild(div);
+    tvguide.content2 = WebLibSimple.createDiv(tvguide.constanst.senderbarWidth, 0, 0, 0, "content2", tvguide.content1);
+    WebLibSimple.setBGColor(tvguide.content2, "#ffff00");
 
     //
     // topdiv.content1.content2.timeline
     //
 
-    var div                   = document.createElement("div");
-    div.id                    = "timeline";
-    div.style.position        = "absolute";
-    div.style.top             = "0px";
-    div.style.left            = "0px";
-    div.style.right           = "0px";
-    div.style.height          = tvguide.constans.timelineHeight + "px";
-    div.style.backgroundColor = "#ff00ff";
-    tvguide.timeline          = div;
+    tvguide.timeline = WebLibSimple.createDivHeight(0, 0, 0, tvguide.constanst.timelineHeight, "timeline", tvguide.content2);
+    tvguide.timeline.style.overflow = "hidden";
 
-    tvguide.content2.appendChild(div);
+    WebLibSimple.setBGColor(tvguide.timeline, "#ff00ff");
 
     //
     // topdiv.content1.content2.epgdata
     //
 
-    var div                   = document.createElement("div");
-    div.id                    = "epgdata";
-    div.style.position        = "absolute";
-    div.style.top             = tvguide.constans.timelineHeight + "px";
-    div.style.left            = "0px";
-    div.style.right           = "0px";
-    div.style.bottom          = "0px";
-    div.style.backgroundColor = "#ffffff";
-    tvguide.epgdata           = div;
-
-    tvguide.content2.appendChild(div);
+    tvguide.epgdata = WebLibSimple.createDiv(0, tvguide.constanst.timelineHeight, 0, 0, "epgdata", tvguide.content2);
+    WebLibSimple.setBGColor(tvguide.epgdata, "#ffffff");
 }
 
 tvguide.createSenderBar = function()
 {
-    var div            = document.createElement("div");
-    div.id             = "senderbarScroll";
-    div.style.position = "absolute";
-    div.style.top      = "0px";
-    div.style.left     = "0px";
-    div.style.right    = "0px";
-
-    tvguide.senderbarScroll = div;
-    tvguide.senderbar.appendChild(div);
+    tvguide.senderbarScroll = WebLibSimple.createDivHeight(0, 0, 0, null, "senderbarScroll", tvguide.senderbar);
+    tvguide.senderbarScroll.scrollVertical = true;
 
     for(var index in tvguide.senderList)
     {
-        var senderDiv            = document.createElement("div");
-        senderDiv.style.position = "absolute";
-        senderDiv.style.top      = tvguide.constans.timelineHeight +
-                                   tvguide.constans.senderHeight * index + "px";
-        senderDiv.style.height   = tvguide.constans.senderHeight + "px";
-        senderDiv.style.width    = tvguide.constans.senderbarWidth + "px";
-        senderDiv.style.left     = "0px";
-        senderDiv.style.right    = "0px";
+        var senderDiv = WebLibSimple.createAnyAppend("div", tvguide.senderbarScroll);
+        senderDiv.style.position = "relative";
+        senderDiv.style.height = tvguide.constanst.senderHeight + "px";
+        senderDiv.style.backgroundColor = "#00ff00";
 
-        var paddingDiv                   = document.createElement("div");
-        paddingDiv.align                 = "center";
-        paddingDiv.style.padding         = "5px";
+        var paddingDiv = WebLibSimple.createAnyAppend("div", senderDiv);
         paddingDiv.style.backgroundColor = "#ed1a79";
+        paddingDiv.style.position        = "absolute";
+        paddingDiv.style.padding         = "7px";
+        paddingDiv.style.left            = "0px";
+        paddingDiv.style.top             = "0px";
+        paddingDiv.style.right           = "0px";
+        paddingDiv.style.bottom          = "0px";
 
-        var image = document.createElement("img");
+        var image = WebLibSimple.createAnyAppend("img", paddingDiv);
+        image.style.height = "100%";
+        image.style.width  = "100%";
 
         image.src = encodeURI("http://" +
-            tvguide.manifest.appserver + "/channels/" +
+            WebApp.manifest.appserver + "/channels/" +
             tvguide.senderList[ index ] + ".png"
         );
+    }
+}
 
-        image.id           = tvguide.senderList[ index ];
-        image.style.top    = "0px";
-        image.style.left   = "0px";
-        image.style.right  = "0px";
-        image.style.bottom = "0px";
-        image.style.height = tvguide.constans.senderHeight + "px";
+tvguide.createTimeLine = function()
+{
+    var hours = 48;
+    var hoursPix = 360
 
+    tvguide.timelineScroll = WebLibSimple.createDivWidth(0, 0, (hours * hoursPix), 0, "timelineScroll", tvguide.timeline);
+    tvguide.timelineScroll.scrollHorizontal = true;
+//    tvguide.timelineScroll.style.backgroundColor = "#ffff00";
 
-        paddingDiv.appendChild(image)
-        senderDiv.appendChild(paddingDiv)
+    for(var inx = 0; inx < hours; inx++)
+    {
+        var timeDiv = WebLibSimple.createAnyAppend("div", tvguide.timelineScroll);
+        timeDiv.style.backgroundColor = "#6ff9ec";
+        timeDiv.style.position = "absolute";
+        timeDiv.style.width    = hoursPix + "px";
+        timeDiv.style.left     = (hoursPix * inx) +"px";
+        timeDiv.style.top      = "0px";
+        timeDiv.style.bottom   = "0px";
 
-        tvguide.senderbarScroll.appendChild(senderDiv);
+        var timeSpan = WebLibSimple.createAnyAppend("span", timeDiv);
+        timeSpan.textContent = inx;
     }
 
-    senderbarScroll.clientHeight = tvguide.senderList.length * tvguide.constans.senderHeight;
-//
-// senderbarScroll
-// senderbarScroll.style.height = x * iconheight;
-// senderbarScroll.clientHeight = x * iconheight;
-// senderbarScroll.clientWidth
-// senderbarScroll.offsetTop
-// senderbarScroll.offsetLeft
-
-    console.log("===============> senderbar height=" + tvguide.senderbar.clientHeight);
+    console.log("===============> tvguide.timelineScroll Width=" + tvguide.timelineScroll.clientWidth);
+    console.log("===============> tvguide.timelineScroll tvguide.timeline.clientWidth=" + tvguide.timeline.clientWidth);
+    console.log("===============> tvguide.timelineScroll hourSize=" + hourSize);
 }
+
 
 tvguide.createFrameSetup();
 tvguide.getSenderList();
 tvguide.createSenderBar();
+tvguide.createTimeLine();
