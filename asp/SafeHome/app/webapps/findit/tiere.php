bf4f1db343ae63e51e77167ff2fb9e70745791b6
Dennis Zierahn <dezi@kappa-mm.de>
2016-03-29 11:09:54 +0200

    Findit data lists and icons.

--- asp/SafeHome/app/webapps/findit/tiere.php
+++ asp/SafeHome/app/webapps/findit/tiere.php
@@ -1,37 +1,113 @@
 <?php
 
+include("/Users/dezi/xavaro/php/include/json.php");
 
+$file = "tiere.json";
 
-function get($az)
+$result = array();
+
+if (file_exists($file))
 {
-	$page = file_get_contents("http://tierdoku.de/index.php?title=Tiere-" . $az);
+    $result = json_decdat(file_get_contents("tiere.json"));
+
+    $index[] = array();
+
+    foreach ($result as $class => $list)
+    {
+        foreach ($list as $inx => $array)
+        {
+            $key = $class . "|" . $array[ 0 ] . "|" . $array[ 1 ];
+            $index[ $key ] = true;
+        }
+    }
+}
+
+for ($buch = 0; $buch <= 26; $buch++)
+{
+	$ccc = chr(ord("A") + $buch);
 	
+	$page = file_get_contents("http://tierdoku.com/index.php?title=Tiere-" . $ccc);
 	$lines = explode("\n", $page);
-	
+
 	for ($inx = 0; $inx < count($lines); $inx++)
 	{
 		$line = $lines[ $inx ];
-		
 		if (substr($line, 0, 13) != "<td> <a href=") continue;
 		
-		$line = substr($line,14);
+		$line = substr($line, 14);
 		$pos1 = strpos($line, ">") + 1;
 		$pos2 = strpos($line, "</a>");
 		
 		$name = substr($line, $pos1, $pos2 - $pos1);
-		$latin = substr($lines[ $inx + 1 ], 13, -4);
-		$class = substr($lines[ $inx + 2 ], 13, -4);
 		
-		$wikiname = str_replace(" ", "_", $name);
-		$wikiurl = "https://de.wikipedia.org/wiki/" . $name;
-		
-		if (@file_get_contents($wikiurl))
-		{
-			echo "$name => $latin => $class => $wikiname\n";
-		}
-	}
-}
+		$latin = substr($lines[ $inx + 1], 13, -4);
+		$class = substr($lines[ $inx + 2], 13, -4);
 
-for ($inx = 0; $inx < 26; $inx++) get(chr(ord("A") + $inx));
+        $key = $class . "|" . $name . "|" . $latin;
+
+        if (isset($index[ $key ]))
+        {
+			echo "OL: $name => $latin => $class\n";
+            continue;
+        }
+
+		$found = false;
+
+		$wikiname = str_replace(" ", "_", $name);
+		$wikiurl = "https://de.wikipedia.org/wiki/" . $wikiname;
+
+		if (($found === false) && @file_get_contents($wikiurl))
+		{
+		    $found = true;
+		}
+
+		if ($found === false)
+		{
+			$wikiname = urlencode(str_replace(" ", "_", $name));
+			$wikiurl = "https://de.wikipedia.org/wiki/" . $wikiname;
+
+			if (@file_get_contents($wikiurl)) $found = true;
+		}
+
+		if ($found === false)
+		{
+			$wikiname = str_replace(" ", "_", $latin);
+			$wikiurl = "https://de.wikipedia.org/wiki/" . $wikiname;
+
+			if (@file_get_contents($wikiurl)) $found = true;
+		}
+
+		if ($found === false)
+		{
+			echo "ER: $name => $latin => $class => $wikiname\n";
+
+			continue;
+		}
+		else
+		{
+			echo "OK: $name => $latin => $class => $wikiname\n";
+		}
+
+		if (! isset($result[ $class ])) $result[ $class ] = array();
+		
+		$data = array();
+		$data[] = $name;
+		$data[] = $latin;
+
+		if (($wikiname == str_replace(" ", "_", $name)) ||
+		    ($wikiname == urlencode(str_replace(" ", "_", $name))))
+		{
+		    $data[] = 1;
+		}
+		else
+		{
+               $data[] = $wikiname;
+		}
+
+		$result[ $class ][] = $data;
+	}
+	
+	file_put_contents($file, json_encdat($result));
+}
 
 ?>
