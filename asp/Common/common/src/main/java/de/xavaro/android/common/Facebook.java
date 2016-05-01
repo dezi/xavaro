8f9e218b28974bc47f4332638cc74179faed2443
dezi <dezi@kappa-mm.de>
2016-05-01 17:44:35 +0200

    Facebook stuff.

--- asp/Common/common/src/main/java/de/xavaro/android/common/Facebook.java
+++ asp/Common/common/src/main/java/de/xavaro/android/common/Facebook.java
@@ -1,10 +1,9 @@
 package de.xavaro.android.common;
 
+import android.support.annotation.Nullable;
 import android.app.Application;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.FacebookSdk;
@@ -20,17 +19,23 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Collection;
 import java.util.Arrays;
+import java.util.Set;
 
 public class Facebook
 {
     private static final String LOGTAG = Facebook.class.getSimpleName();
 
     private static final Collection<String> permissions = Arrays.asList
-            ("public_profile", "user_friends", "user_posts");
+    (
+        "public_profile",
+        "user_friends",
+        "user_posts"
+    );
 
     private static final GraphRequest graphrequest = new GraphRequest();
     private static GraphResponse response;
@@ -81,6 +86,7 @@ public class Facebook
         public void onCompleted(GraphResponse response)
         {
             Log.d(LOGTAG, "graphcallback: onCompleted..." + response);
+
             Facebook.response = response;
         }
     };
@@ -121,37 +127,75 @@ public class Facebook
         LoginManager.getInstance().logInWithReadPermissions(Simple.getActContext(), permissions);
     }
 
+    public static void logout()
+    {
+        LoginManager.getInstance().logOut();
+    }
+
     public static boolean isLoggedIn()
     {
+        if (AccessToken.getCurrentAccessToken() != null)
+        {
+            Log.d(LOGTAG, "isLoggedIn: permissions=" + AccessToken.getCurrentAccessToken().getPermissions());
+        }
+
         return (AccessToken.getCurrentAccessToken() != null) &&
                 (Profile.getCurrentProfile() != null);
     }
 
     @Nullable
-    public static String getProfileId()
+    public static String getUserId()
     {
-        Profile profile = Profile.getCurrentProfile();
-        return (profile == null) ? null : profile.getId();
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        return (token == null) ? null : token.getUserId();
     }
 
     @Nullable
-    public static String getProfileDisplayName()
+    public static Set<String> getUserPermissions()
     {
-        Profile profile = Profile.getCurrentProfile();
-        return (profile == null) ? null : profile.getName();
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        return token.getPermissions();
     }
 
     @Nullable
-    public static String getProfileExpiration()
+    public static String getUserTokenExpiration()
     {
         AccessToken token = AccessToken.getCurrentAccessToken();
         return (token == null) ? null : Simple.timeStampAsISO(token.getExpires().getTime());
     }
 
     @Nullable
-    public static byte[] getUserIcon(String facebookid)
+    public static String getUserDisplayName()
     {
-        if (facebookid == null) return null;
+        Profile profile = Profile.getCurrentProfile();
+        return (profile == null) ? null : profile.getName();
+    }
+
+    @Nullable
+    public static JSONArray getUserFriendlist()
+    {
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        if (token == null) return null;
+
+        Bundle parameters = new Bundle();
+
+        //graphrequest.setAccessToken(AccessToken.getCurrentAccessToken());
+        graphrequest.setGraphPath("/" + getUserId() + "/friends");
+        graphrequest.setParameters(parameters);
+        graphrequest.executeAndWait();
+
+        JSONObject json = response.getJSONObject();
+        JSONArray data = Json.getArray(json, "data");
+
+        if (data != null) Log.d(LOGTAG, "getUserFriendlists: data:" + data.toString());
+
+        return data;
+    }
+
+    @Nullable
+    public static byte[] getUserIcon(String fbid)
+    {
+        if (fbid == null) return null;
 
         AccessToken token = AccessToken.getCurrentAccessToken();
         if (token == null) return null;
@@ -162,7 +206,7 @@ public class Facebook
         parameters.putInt("width", 400);
         parameters.putInt("height", 400);
 
-        graphrequest.setGraphPath("/" + facebookid + "/picture");
+        graphrequest.setGraphPath("/" + fbid + "/picture");
         graphrequest.setParameters(parameters);
         graphrequest.executeAndWait();
 
@@ -170,7 +214,7 @@ public class Facebook
         JSONObject data = Json.getObject(json, "data");
         String iconurl = Json.getString(data, "url");
 
-        Log.d(LOGTAG, "getUserIcon: facebookid:" + facebookid + "=" + iconurl);
+        Log.d(LOGTAG, "getUserIcon: facebookid:" + fbid + "=" + iconurl);
 
         return SimpleRequest.readData(iconurl);
     }
--- asp/SafeHome/app/src/main/java/de/xavaro/android/safehome/PreferencesSocial.java
+++ asp/SafeHome/app/src/main/java/de/xavaro/android/safehome/PreferencesSocial.java
@@ -3,9 +3,17 @@ package de.xavaro.android.safehome;
 import android.content.Context;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 import de.xavaro.android.common.CommonConfigs;
 import de.xavaro.android.common.Facebook;
+import de.xavaro.android.common.Json;
 import de.xavaro.android.common.NicedPreferences;
 import de.xavaro.android.common.PreferenceFragments;
 import de.xavaro.android.common.ProfileImages;
@@ -42,54 +50,175 @@ public class PreferencesSocial
 
         NicedPreferences.NiceCategoryPreference facebookHead;
         NicedPreferences.NiceDisplayTextPreference facebookUser;
-        NicedPreferences.NiceDisplayTextPreference facebookToken;
+        NicedPreferences.NiceDisplayTextPreference facebookExpi;
 
         public void registerAll(Context context)
         {
             super.registerAll(context);
 
             NicedPreferences.NiceDisplayTextPreference dp;
+            NicedPreferences.NiceDeletePreference xp;
+            NicedPreferences.NiceCheckboxPreference cp;
 
-            String userid = Facebook.getProfileId();
+            //
+            // Get login status. Store current credentials
+            // into or get last loggedin credentials from
+            // preferences.
+            //
+
+            String fbid = Facebook.getUserId();
+            String name = Facebook.getUserDisplayName();
+            String expi = Facebook.getUserTokenExpiration();
+
+            if (fbid != null) Simple.setSharedPrefString("facebook.fbid", fbid);
+            if (name != null) Simple.setSharedPrefString("facebook.name", name);
+            if (expi != null) Simple.setSharedPrefString("facebook.expi", expi);
+
+            if (fbid == null) fbid = Simple.getSharedPrefString("facebook.fbid");
+            if (name == null) name = Simple.getSharedPrefString("facebook.name");
+            if (expi == null) expi = Simple.getSharedPrefString("facebook.expi");
 
             facebookHead = new NicedPreferences.NiceInfoPreference(context);
             facebookHead.setTitle("Facebook Account");
             facebookHead.setSummary(R.string.pref_basic_owner_profile_summary);
-            facebookHead.setIcon(ProfileImages.getFacebookProfileDrawable(userid, true));
+            facebookHead.setIcon(ProfileImages.getFacebookProfileDrawable(fbid, true));
+            facebookHead.setEnabled(enabled);
 
             preferences.add(facebookHead);
-
-            String name = Facebook.getProfileDisplayName();
 
             facebookUser = new NicedPreferences.NiceDisplayTextPreference(context);
             facebookUser.setTitle("Facebook Benutzer");
             facebookUser.setText(name);
+            facebookUser.setEnabled(enabled);
 
             preferences.add(facebookUser);
 
-            String expi = Facebook.getProfileExpiration();
+            facebookExpi = new NicedPreferences.NiceDisplayTextPreference(context);
+            facebookExpi.setTitle("Gültig bis");
+            facebookExpi.setText((expi == null) ? null : Simple.getLocalDateLong(expi));
+            facebookExpi.setEnabled(enabled);
 
-            facebookToken = new NicedPreferences.NiceDisplayTextPreference(context);
-            facebookToken.setTitle("Eingeloggt bis");
-            facebookToken.setText((expi == null) ? null : Simple.getLocalDateLong(expi));
-
-            preferences.add(facebookToken);
+            preferences.add(facebookExpi);
 
             dp = new NicedPreferences.NiceDisplayTextPreference(context);
-            dp.setTitle(Facebook.isLoggedIn()? "Account wechseln" : "Account festlegen");
+            dp.setTitle(Facebook.isLoggedIn() ? "Ausloggen" : "Einloggen");
+            dp.setText(Facebook.isLoggedIn() ? "ist eingeloggt" : "ist ausgelogt");
+            dp.setEnabled(enabled);
 
             dp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
             {
                 @Override
                 public boolean onPreferenceClick(Preference preference)
                 {
-                    Facebook.login();
+                    if (Facebook.isLoggedIn())
+                    {
+                        Facebook.logout();
+                    }
+                    else
+                    {
+                        Facebook.login();
+                    }
 
                     return false;
                 }
             });
 
             preferences.add(dp);
+
+            dp = new NicedPreferences.NiceDisplayTextPreference(context);
+            dp.setTitle(Facebook.isLoggedIn() ? "Etwas testen" : "Nix machen");
+            dp.setEnabled(enabled);
+
+            dp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
+            {
+                @Override
+                public boolean onPreferenceClick(Preference preference)
+                {
+                    Log.d(LOGTAG, "Permissions=" + Facebook.getUserPermissions());
+                    Facebook.getUserFriendlist();
+                    return false;
+                }
+            });
+
+            preferences.add(dp);
+
+            //
+            // Display already known friends list.
+            //
+
+            ArrayList<String> oldfriends = new ArrayList<>();
+
+            String friendsname = "facebook.friend.name.";
+            String friendsenabled = "facebook.friend.enabled.";
+
+            Map<String, Object> oldfriendspref = Simple.getAllPreferences(friendsname);
+
+            for (Map.Entry<String, ?> entry : oldfriendspref.entrySet())
+            {
+                Object fnobj = entry.getValue();
+
+                if (! (fnobj instanceof String)) continue;
+
+                String ffbid = entry.getKey().substring(friendsname.length());
+                String fname = (String) fnobj;
+
+                xp = new NicedPreferences.NiceDeletePreference(context);
+
+                xp.setTitle(fname);
+                xp.setIcon(ProfileImages.getFacebookProfileDrawable(ffbid, true));
+                xp.setEnabled(enabled);
+
+                preferences.add(xp);
+
+                cp = new NicedPreferences.NiceCheckboxPreference(context);
+                cp.setKey(friendsenabled + ffbid);
+                cp.setTitle("Aktiviert");
+                cp.setEnabled(enabled);
+
+                preferences.add(cp);
+                oldfriends.add(ffbid);
+            }
+
+            //
+            // Display new potential friends list.
+            //
+
+            if (Facebook.isLoggedIn())
+            {
+                JSONArray allfriends = Facebook.getUserFriendlist();
+
+                if (allfriends != null)
+                {
+                    for (int inx = 0; inx < allfriends.length(); inx++)
+                    {
+                        JSONObject allfriend = Json.getObject(allfriends, inx);
+                        if (allfriend == null) continue;
+
+                        String ffbid = Json.getString(allfriend, "id");
+                        String fname = Json.getString(allfriend, "name");
+
+                        if (oldfriends.contains(ffbid)) continue;
+
+                        Simple.setSharedPrefString(friendsname + ffbid, fname);
+
+                        xp = new NicedPreferences.NiceDeletePreference(context);
+
+                        xp.setTitle(fname);
+                        xp.setIcon(ProfileImages.getFacebookProfileDrawable(ffbid, true));
+                        xp.setEnabled(enabled);
+
+                        preferences.add(xp);
+
+                        cp = new NicedPreferences.NiceCheckboxPreference(context);
+                        cp.setKey(friendsenabled + ffbid);
+                        cp.setTitle("Einbeziehen");
+                        cp.setDefaultValue(true);
+                        cp.setEnabled(enabled);
+
+                        preferences.add(cp);
+                    }
+                }
+            }
         }
     }
 
