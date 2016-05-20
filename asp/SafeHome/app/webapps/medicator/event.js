14abff1d1aa3e18bbd35409bc9f7bb6478534264
Dennis Zierahn <dezi@kappa-mm.de>
2016-05-20 15:56:13 +0200

    Notifications.

--- asp/SafeHome/app/webapps/medicator/event.js
+++ asp/SafeHome/app/webapps/medicator/event.js
@@ -3,88 +3,106 @@ medicator.remindIntervall = 600;
 
 medicator.getNextEvents = function()
 {
-    if (medicator.requestUnloadTimer)
-    {
-        clearTimeout(medicator.requestUnloadTimer);
-        medicator.requestUnloadTimer = null;
-    }
-
     var events = JSON.parse(WebAppEvents.getCurrentEventsClear());
 
-    var configs = {};
-
-    //
-    // Preset event types to avoid reminding the same
-    // type of medication twice in one run.
-    //
-
-    medicator.pillsreminder = false;
-    medicator.weightreminder = false;
-    medicator.bloodglucosereminder = false;
-    medicator.bloodpressurereminder = false;
-
-    for (var inx = 0; inx < events.length; inx++)
+    if (events.length > 0)
     {
-        var event = events[ inx ];
-
         //
-        // The formkey collects medication event into the same
-        // time slot, while activity events are kept separate.
+        // Cancel unload request timer because we have events.
         //
 
-        var mediform = event.medication.substring(event.medication.length - 3);
-        var mediflat = (mediform.startsWith("ZZ") ? mediform : "AAA")
-        var formkey = event.date + ":" + mediflat;
-
-        if (! configs[ formkey ])
+        if (medicator.requestUnloadTimer)
         {
+            clearTimeout(medicator.requestUnloadTimer);
+            medicator.requestUnloadTimer = null;
+        }
+
+        //
+        // Work on events.
+        //
+
+        var configs = {};
+
+        //
+        // Preset event types to avoid reminding the same
+        // type of medication twice in one run.
+        //
+
+        medicator.pillsreminder = false;
+        medicator.weightreminder = false;
+        medicator.bloodglucosereminder = false;
+        medicator.bloodpressurereminder = false;
+
+        for (var inx = 0; inx < events.length; inx++)
+        {
+            var event = events[ inx ];
+
             //
-            // Create a new launch item for this event.
+            // The formkey collects medication event into the same
+            // time slot, while activity events are kept separate.
             //
 
-            var config = {};
-            config.date = event.date;
-            config.mediflat = mediflat;
-            config.mediform = mediform;
-            config.events = [];
+            var mediform = event.medication.substring(event.medication.length - 3);
+            var mediflat = (mediform.startsWith("ZZ") ? mediform : "AAA")
+            var formkey = event.date + ":" + mediflat;
 
-            configs[ formkey ] = config;
+            if (! configs[ formkey ])
+            {
+                //
+                // Create a new launch item for this event.
+                //
+
+                var config = {};
+                config.date = event.date;
+                config.mediflat = mediflat;
+                config.mediform = mediform;
+                config.events = [];
+
+                configs[ formkey ] = config;
+            }
+
+            configs[ formkey ].events.push(event);
         }
 
-        configs[ formkey ].events.push(event);
-    }
-
-    var action = false;
-
-    for (var formkey in configs)
-    {
-        var config = configs[ formkey ];
-
-        console.log("config=" + JSON.stringify(config));
-
-        var alltaken = true;
-        var ondemand = true;
-
-        for (var inx in config.events)
+        for (var formkey in configs)
         {
-            var event = config.events[ inx ];
+            var config = configs[ formkey ];
 
-            alltaken = alltaken && event.taken;
-            ondemand = ondemand && event.ondemand;
+            console.log("config=" + JSON.stringify(config));
+
+            var alltaken = true;
+            var ondemand = true;
+
+            for (var inx in config.events)
+            {
+                var event = config.events[ inx ];
+
+                alltaken = alltaken && event.taken;
+                ondemand = ondemand && event.ondemand;
+            }
+
+            if (alltaken || ondemand)
+            {
+                medicator.completeConfig(config);
+
+                continue;
+            }
+
+            medicator.remindConfig(config);
         }
-
-        if (alltaken || ondemand)
-        {
-            medicator.completeConfig(config);
-
-            continue;
-        }
-
-        if (medicator.remindConfig(config)) action = true;
     }
 
     medicator.getNextEventsTimer = setTimeout(medicator.getNextEvents,  10 * 1000);
-    medicator.requestUnloadTimer = setTimeout(medicator.requestUnload, 100 * 1000);
+
+    if (! medicator.requestUnloadTimer)
+    {
+        //
+        // Schedule unload request timer which will fire after
+        // the given time if there are no events anymore.
+        //
+
+        medicator.requestUnloadTimer = setTimeout(medicator.requestUnload, 100 * 1000);
+    }
 }
 
 medicator.requestUnload = function()
