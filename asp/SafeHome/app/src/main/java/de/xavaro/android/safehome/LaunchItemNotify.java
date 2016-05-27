8e3de87f221f1e87a0c74a03e862701fbea4307c
Dennis Zierahn <dezi@kappa-mm.de>
2016-05-27 13:33:11 +0200

    Notifications.

--- asp/SafeHome/app/src/main/java/de/xavaro/android/safehome/LaunchItemNotify.java
+++ asp/SafeHome/app/src/main/java/de/xavaro/android/safehome/LaunchItemNotify.java
@@ -1,9 +1,12 @@
 package de.xavaro.android.safehome;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import de.xavaro.android.common.Simple;
 import de.xavaro.android.common.Json;
@@ -17,6 +20,72 @@ public class LaunchItemNotify extends LaunchItem
         super(context);
     }
 
+    protected final ArrayList<String> dupscount = new ArrayList<>();
+
+    protected int mainnews;
+    protected int totalnews;
+
+    private void subscribeLaunchItems(JSONObject config, boolean subscribe)
+    {
+        JSONArray launchItems = Json.getArray(config, "launchitems");
+
+        if (launchItems != null)
+        {
+            for (int inx = 0; inx < launchItems.length(); inx++)
+            {
+                JSONObject liconfig = Json.getObject(launchItems, inx);
+                if (liconfig == null) continue;
+
+                if (subscribe)
+                {
+                    LaunchItem.subscribeNotification(liconfig, onNotification);
+                }
+                else
+                {
+                    LaunchItem.unsubscribeNotification(liconfig, onNotification);
+                }
+
+                if (liconfig.has("launchitems"))
+                {
+                    subscribeLaunchItems(liconfig, subscribe);
+                }
+            }
+        }
+    }
+
+    private void countEventsLaunchItems(JSONObject config, String mainitemkey)
+    {
+        JSONArray launchItems = Json.getArray(config, "launchitems");
+
+        if (launchItems != null)
+        {
+            for (int inx = 0; inx < launchItems.length(); inx++)
+            {
+                JSONObject liconfig = Json.getObject(launchItems, inx);
+                if (liconfig == null) continue;
+
+                String subitemkey = LaunchItem.getNotificationKey(liconfig);
+
+                if (! dupscount.contains(subitemkey))
+                {
+                    dupscount.add(subitemkey);
+
+                    int subnews = LaunchItem.getNotificationCount(liconfig);
+                    if (subnews < 0) continue;
+
+                    totalnews += subnews;
+
+                    if (Simple.equals(mainitemkey, subitemkey))
+                    {
+                        mainnews += subnews;
+                    }
+                }
+
+                if (liconfig.has("launchitems")) countEventsLaunchItems(liconfig, mainitemkey);
+            }
+        }
+    }
+
     @Override
     protected void onAttachedToWindow()
     {
@@ -24,18 +93,7 @@ public class LaunchItemNotify extends LaunchItem
 
         if (config.has("launchitems"))
         {
-            JSONArray launchItems = Json.getArray(config, "launchitems");
-
-            if (launchItems != null)
-            {
-                for (int inx = 0; inx < launchItems.length(); inx++)
-                {
-                    JSONObject liconfig = Json.getObject(launchItems, inx);
-                    if (liconfig == null) continue;
-
-                    LaunchItem.subscribeNotification(liconfig, onNotification);
-                }
-            }
+            subscribeLaunchItems(config, true);
         }
         else
         {
@@ -53,18 +111,7 @@ public class LaunchItemNotify extends LaunchItem
 
         if (config.has("launchitems"))
         {
-            JSONArray launchItems = Json.getArray(config, "launchitems");
-
-            if (launchItems != null)
-            {
-                for (int inx = 0; inx < launchItems.length(); inx++)
-                {
-                    JSONObject liconfig = Json.getObject(launchItems, inx);
-                    if (liconfig == null) continue;
-
-                    LaunchItem.unsubscribeNotification(liconfig, onNotification);
-                }
-            }
+            subscribeLaunchItems(config, false);
         }
         else
         {
@@ -75,9 +122,6 @@ public class LaunchItemNotify extends LaunchItem
         }
     }
 
-    protected int mainnews;
-    protected int totalnews;
-
     protected final Runnable onNotification = new Runnable()
     {
         @Override
@@ -85,53 +129,32 @@ public class LaunchItemNotify extends LaunchItem
         {
             if (config.has("launchitems"))
             {
-                JSONArray launchItems = Json.getArray(config, "launchitems");
+                mainnews = 0;
+                totalnews = 0;
+                dupscount.clear();
 
-                if (launchItems != null)
+                String mainitemkey = LaunchItem.getNotificationKey(config);
+                countEventsLaunchItems(config, mainitemkey);
+
+                if (totalnews >= 0)
                 {
-                    String mainitemkey = LaunchItem.getNotificationKey(config);
+                    int mesgresid = LaunchItem.getSubscribeNotificationMessage(config);
 
-                    mainnews = 0;
-                    totalnews = 0;
-
-                    for (int inx = 0; inx < launchItems.length(); inx++)
+                    if (totalnews != mainnews)
                     {
-                        JSONObject liconfig = Json.getObject(launchItems, inx);
-                        if (liconfig == null) continue;
+                        //
+                        // We have notifications from different areas.
+                        // Adjust message to be "news".
+                        //
 
-                        int subnews = LaunchItem.getNotificationCount(liconfig);
-                        if (subnews < 0) continue;
-
-                        totalnews += subnews;
-
-                        String subitemkey = LaunchItem.getNotificationKey(liconfig);
-
-                        if (Simple.equals(mainitemkey, subitemkey))
-                        {
-                            mainnews += subnews;
-                        }
+                        mesgresid = R.string.simple_news;
                     }
 
-                    if (totalnews >= 0)
-                    {
-                        int mesgresid = LaunchItem.getSubscribeNotificationMessage(config);
+                    String message = totalnews + " " + Simple.getTrans(
+                            (totalnews == 1) ? mesgresid : Simple.getPlural(mesgresid));
 
-                        if (totalnews != mainnews)
-                        {
-                            //
-                            // We have notifications from different areas.
-                            // Adjust message to be "news".
-                            //
-
-                            mesgresid = R.string.simple_news;
-                        }
-
-                        String message = totalnews + " " + Simple.getTrans(
-                                (totalnews == 1) ? mesgresid : Simple.getPlural(mesgresid));
-
-                        notifyText.setText(message);
-                        notifyText.setVisibility((totalnews == 0) ? GONE : VISIBLE);
-                    }
+                    notifyText.setText(message);
+                    notifyText.setVisibility((totalnews == 0) ? GONE : VISIBLE);
                 }
             }
             else
