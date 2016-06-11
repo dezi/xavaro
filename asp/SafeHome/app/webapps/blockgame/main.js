885aec11804312ec11c73a7dae01471b3d80acd6
dezi <dezi@kappa-mm.de>
2016-06-11 07:49:55 +0200

    Blockgame.

--- asp/SafeHome/app/webapps/blockgame/main.js
+++ asp/SafeHome/app/webapps/blockgame/main.js
@@ -23,17 +23,186 @@ blockgame.createFrame = function()
 
     WebLibSimple.setBGColor(xx.gamePanel, "#ffdddd");
 
+    xx.buttonTop1div = blockgame.createButton("–");
+    xx.buttonTop2div = blockgame.createButton(" ");
+    xx.buttonTop3div = blockgame.createButton("+");
+
+    xx.buttonBot1div = blockgame.createButton("!");
+    xx.buttonBot2div = blockgame.createButton(" ");
+    xx.buttonBot3div = blockgame.createButton("?");
+
+    xx.buttonTop1div.onTouchClick = blockgame.onButtonMinus;
+    xx.buttonTop3div.onTouchClick = blockgame.onButtonPlus;
+    xx.buttonBot1div.onTouchClick = blockgame.onLoadNewGame;
+
     xx.audio = WebLibSimple.createAnyAppend("audio", null);
-    xx.audio.src = "/webapps/blockgame/sounds/finish_level_2.ogg";
+    xx.audio.src = "/webapps/blockgame/sounds/finish_level_2.wav";
     xx.audio.preload = "auto";
+
+    xx.onWindowResize();
+
+    addEventListener("resize", blockgame.onWindowResize);
+}
+
+blockgame.createButton = function(text)
+{
+    var buttonDiv;
+    var buttonImg;
+
+    buttonDiv = WebLibSimple.createDivWidHei(null, null, 128, 128, null, blockgame.topdiv);
+
+    buttonImg = WebLibSimple.createAnyAppend("img", buttonDiv);
+    buttonImg.style.width  = "100%";
+    buttonImg.style.height = "100%";
+    buttonImg.src = "black_button_256x256.png";
+
+    buttonTab = WebLibSimple.createDivWidHei(0, 0, "100%", "100%", null, buttonDiv);
+    buttonTab.style.display = "table";
+
+    buttonCen = WebLibSimple.createAnyAppend("div", buttonTab);
+    WebLibSimple.setFontSpecs(buttonCen, 64, "bold");
+    buttonCen.style.display = "table-cell";
+    buttonCen.style.width   = "100%";
+    buttonCen.style.height  = "100%";
+    buttonCen.style.color   = "#ffffff";
+    buttonCen.style.verticalAlign = "middle";
+    buttonCen.style.textAlign     = "center";
+    buttonCen.innerHTML = text;
+
+    buttonDiv.buttonCen = buttonCen;
+
+    return buttonDiv;
+}
+
+blockgame.loadNewGameLevel = function()
+{
+    blockgame.newLevelTimeout = null;
+    blockgame.readLevel(blockgame.level);
+    blockgame.buildGame();
+}
+
+blockgame.onLoadNewGame = function(target, ctarget)
+{
+    WebAppUtility.makeClick();
+
+    blockgame.buildGame();
+}
+
+blockgame.onButtonMinus = function(target, ctarget)
+{
+    var xx = blockgame;
+
+    WebAppUtility.makeClick();
+
+    xx.buttonTop2div.buttonCen.innerHTML = --xx.level;
+
+    if (xx.newLevelTimeout) clearTimeout(xx.newLevelTimeout);
+    xx.newLevelTimeout = setTimeout(xx.loadNewGameLevel, 1000);
+}
+
+blockgame.onButtonPlus = function(target, ctarget)
+{
+    var xx = blockgame;
+
+    WebAppUtility.makeClick();
+
+    xx.buttonTop2div.buttonCen.innerHTML = ++xx.level;
+
+    if (xx.newLevelTimeout) clearTimeout(xx.newLevelTimeout);
+    xx.newLevelTimeout = setTimeout(xx.loadNewGameLevel, 1000);
+}
+
+blockgame.onWindowResize = function()
+{
+    var xx = blockgame;
+
+    var wid = xx.topdiv.clientWidth;
+    var hei = xx.topdiv.clientHeight;
+
+    var spacehorz = (wid - xx.gamesize) >> 1;
+    var spacevert = (hei - xx.gamesize) >> 1;
+
+    if (spacehorz < spacevert)
+    {
+        var spacevert = (spacevert - xx.buttonTop1div.clientHeight) >> 1;
+        var spacemiddle = (wid >> 1) - (xx.buttonTop1div.clientWidth >> 1);
+
+        xx.buttonTop1div.style.left   = spacehorz + "px";
+        xx.buttonTop1div.style.top    = spacevert + "px";
+        xx.buttonTop1div.style.right  = null;
+        xx.buttonTop1div.style.bottom = null;
+
+        xx.buttonTop2div.style.left   = spacemiddle + "px";
+        xx.buttonTop2div.style.top    = spacevert + "px";
+        xx.buttonTop2div.style.right  = null;
+        xx.buttonTop2div.style.bottom = null;
+
+        xx.buttonTop3div.style.left   = null;
+        xx.buttonTop3div.style.top    = spacevert + "px";
+        xx.buttonTop3div.style.right  = spacehorz + "px";
+        xx.buttonTop3div.style.bottom = null;
+
+        xx.buttonBot1div.style.left   = spacehorz + "px";
+        xx.buttonBot1div.style.top    = null;
+        xx.buttonBot1div.style.right  = null;
+        xx.buttonBot1div.style.bottom = spacevert + "px";
+
+        xx.buttonBot2div.style.left   = spacemiddle + "px";
+        xx.buttonBot2div.style.top    = null;
+        xx.buttonBot2div.style.right  = null;
+        xx.buttonBot2div.style.bottom = spacevert + "px";
+
+        xx.buttonBot3div.style.left   = null;
+        xx.buttonBot3div.style.top    = null;
+        xx.buttonBot3div.style.right  = spacehorz + "px";
+        xx.buttonBot3div.style.bottom = spacevert + "px";
+    }
+    else
+    {
+        var spacehorz = (spacehorz - xx.buttonTop1div.clientWidth) >> 1;
+        var spacemiddle = (hei >> 1) - (xx.buttonTop1div.clientHeight >> 1);
+
+        xx.buttonTop1div.style.left   = spacehorz + "px";
+        xx.buttonTop1div.style.top    = spacevert + "px";
+        xx.buttonTop1div.style.right  = null;
+        xx.buttonTop1div.style.bottom = null;
+
+        xx.buttonTop2div.style.left   = spacehorz + "px";
+        xx.buttonTop2div.style.top    = spacemiddle + "px";
+        xx.buttonTop2div.style.right  = null;
+        xx.buttonTop2div.style.bottom = null;
+
+        xx.buttonTop3div.style.left   = spacehorz + "px";
+        xx.buttonTop3div.style.top    = null;
+        xx.buttonTop3div.style.right  = null;
+        xx.buttonTop3div.style.bottom = spacevert + "px";
+
+        xx.buttonBot1div.style.left   = null;
+        xx.buttonBot1div.style.top    = spacevert + "px";
+        xx.buttonBot1div.style.right  = spacehorz + "px";
+        xx.buttonBot1div.style.bottom = null;
+
+        xx.buttonBot2div.style.left   = null;
+        xx.buttonBot2div.style.top    = spacemiddle + "px";
+        xx.buttonBot2div.style.right  = spacehorz + "px";
+        xx.buttonBot2div.style.bottom = null;
+
+        xx.buttonBot3div.style.left   = null;
+        xx.buttonBot3div.style.top    = null;
+        xx.buttonBot3div.style.right  = spacehorz + "px";
+        xx.buttonBot3div.style.bottom = spacevert + "px";
+    }
 }
 
 blockgame.readLevel = function(level)
 {
     var xx = blockgame;
 
+    xx.level = level;
+    xx.buttonTop2div.buttonCen.innerHTML = level;
+
     var lstring = ((level < 10) ? "0" : "") + level;
-    var url = "/games/level." + lstring + ".txt";
+    var url = "games/level." + lstring + ".txt";
     var leveldata = WebAppRequest.loadSync(url);
 
     xx.games = leveldata.trim().split("\n");
@@ -235,15 +404,27 @@ blockgame.onTouchEnd = function(event)
     target.myblock.pos = (ypos * 6) + xpos;
 
     xx.moveBlock(target.myblock);
-    xx.solveGame(xx.game, xx.blocks);
+    xx.clearHint();
 
-    if (! xx.getHint())
+    if (xx.solvepath.length)
+    {
+        setTimeout(blockgame.solveAndHint, 10);
+    }
+    else
     {
         setTimeout(blockgame.playGameEnd, 10);
     }
 
     event.preventDefault();
     return true;
+}
+
+blockgame.solveAndHint = function()
+{
+    var xx = blockgame;
+
+    xx.solveGame(xx.game, xx.blocks);
+    xx.getHint();
 }
 
 blockgame.playGameEnd = function()
@@ -288,6 +469,17 @@ blockgame.moveBlock = function(block)
     xx.game = game.join('');
 }
 
+blockgame.clearHint = function()
+{
+    var xx = blockgame;
+
+    for (var ccc in xx.blocks)
+    {
+        var block = xx.blocks[ ccc ];
+        block.blockcen.innerHTML = "";
+    }
+}
+
 blockgame.getHint = function()
 {
     var xx = blockgame;
@@ -303,15 +495,9 @@ blockgame.getHint = function()
         {
             block.blockcen.innerHTML = nextmove.way + " (" + movescnt + ")";
         }
-        else
-        {
-            block.blockcen.innerHTML = "";
-        }
     }
 
-    console.log("==================>>>>>>>>nexthint=" + JSON.stringify(nextmove));
-
-    return nextmove.ccc;
+    xx.buttonBot2div.buttonCen.innerHTML = movescnt;
 }
 
 blockgame.buildGame = function()
@@ -419,7 +605,5 @@ blockgame.buildGame = function()
 }
 
 blockgame.createFrame();
-blockgame.readLevel(41);
-blockgame.readLevel(33);
-blockgame.readLevel(6);
+blockgame.readLevel(14);
 blockgame.buildGame();
