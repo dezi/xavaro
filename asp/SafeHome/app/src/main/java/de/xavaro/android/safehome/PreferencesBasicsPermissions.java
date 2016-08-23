15683cc3cdadd4e82aae8d554d2b319ce79e8a5d
Dennis Zierahn <dezi@kappa-mm.de>
2016-08-23 15:04:24 +0200

    Check in.

--- asp/SafeHome/app/src/main/java/de/xavaro/android/safehome/PreferencesBasicsPermissions.java
+++ asp/SafeHome/app/src/main/java/de/xavaro/android/safehome/PreferencesBasicsPermissions.java
@@ -2,13 +2,23 @@ package de.xavaro.android.safehome;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import de.xavaro.android.common.CommonConfigs;
 import de.xavaro.android.common.NicedPreferences;
+import de.xavaro.android.common.Owner;
 import de.xavaro.android.common.PreferenceFragments;
+import de.xavaro.android.common.ProfileImages;
 import de.xavaro.android.common.Simple;
 
 public class PreferencesBasicsPermissions extends PreferenceFragments.BasicFragmentStub
@@ -34,6 +44,10 @@ public class PreferencesBasicsPermissions extends PreferenceFragments.BasicFragm
         iconres = CommonConfigs.IconResPermissions;
         summaryres = R.string.pref_basic_permissions_summary;
     }
+
+    public NicedPreferences.NiceListPreference PrefContactsRead;
+    public NicedPreferences.NiceListPreference PrefContactsWrite;
+    public NicedPreferences.NiceListPreference PrefSystemAlertWindow;
 
     public void registerAll(Context context)
     {
@@ -62,9 +76,22 @@ public class PreferencesBasicsPermissions extends PreferenceFragments.BasicFragm
         lp.setTitle(R.string.pref_basic_permissions_contacts_read);
         lp.setEntries(permStatusText);
         lp.setEntryValues(permStatusKeys);
-        lp.setValue(getPermission(Manifest.permission.READ_CONTACTS));
+
+        lp.setOnclick(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Log.d(LOGTAG, "Manifest.permission.READ_CONTACTS...");
+
+                ActivityCompat.requestPermissions(Simple.getActContext(),
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        1234);
+            }
+        });
 
         preferences.add(lp);
+        PrefContactsRead = lp;
 
         lp = new NicedPreferences.NiceListPreference(context);
 
@@ -72,15 +99,101 @@ public class PreferencesBasicsPermissions extends PreferenceFragments.BasicFragm
         lp.setTitle(R.string.pref_basic_permissions_contacts_write);
         lp.setEntries(permStatusText);
         lp.setEntryValues(permStatusKeys);
-        lp.setValue(getPermission(Manifest.permission.WRITE_CONTACTS));
 
         preferences.add(lp);
+        PrefContactsWrite = lp;
+
+        //
+        // System alerts permissions.
+        //
+
+        ip = new NicedPreferences.NiceInfoPreference(context);
+        ip.setTitle(R.string.pref_basic_permissions_system_alert);
+        ip.setSummary(R.string.pref_basic_permissions_system_alert_summary);
+        preferences.add(ip);
+
+        lp = new NicedPreferences.NiceListPreference(context);
+
+        lp.setKey(permprefix + ".system.alert_window");
+        lp.setTitle(R.string.pref_basic_permissions_system_alert_window);
+        lp.setEntries(permStatusText);
+        lp.setEntryValues(permStatusKeys);
+
+        lp.setOnclick(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                ActivityCompat.requestPermissions(Simple.getActContext(),
+                        new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW},
+                        1234);
+            }
+        });
+
+        preferences.add(lp);
+        PrefSystemAlertWindow = lp;
+
+        //
+        // Run preferences monitor.
+        //
+
+        checkPretty.run();
     }
 
-    private static String getPermission(String permission)
+    private static void requestPermission(String permission)
     {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (permission.equals(Manifest.permission.SYSTEM_ALERT_WINDOW))
+            {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + Simple.getAppContext().getPackageName()));
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                Simple.getAppContext().startActivity(intent);
+
+                return;
+            }
+        }
+
+        ActivityCompat.requestPermissions(Simple.getActContext(), new String[]{permission}, 4711);
+    }
+
+    private static String checkPermission(String permission)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (permission.equals(Manifest.permission.SYSTEM_ALERT_WINDOW))
+            {
+                return Settings.canDrawOverlays(Simple.getAppContext()) ? "grant" : "deny";
+            }
+        }
+
         int res = ContextCompat.checkSelfPermission(Simple.getActContext(), permission);
 
-        return  (res == PackageManager.PERMISSION_GRANTED) ? "deny" : "grant";
+        return  (res == PackageManager.PERMISSION_GRANTED) ? "grant" : "deny";
     }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+
+        Simple.removePost(checkPretty);
+    }
+
+    private final Runnable checkPretty = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            PrefContactsRead.setValue(checkPermission(Manifest.permission.READ_CONTACTS));
+            PrefContactsWrite.setValue(checkPermission(Manifest.permission.WRITE_CONTACTS));
+            PrefSystemAlertWindow.setValue(checkPermission(Manifest.permission.SYSTEM_ALERT_WINDOW));
+
+            Simple.makePost(checkPretty, 1 * 1000);
+        }
+    };
+
 }
