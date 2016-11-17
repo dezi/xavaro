86085c7d217d0b85802b1ab52a0ef8f2d465dbff
Dennis Zierahn <dezi@kappa-mm.de>
2016-11-17 15:37:18 +0100

    Check in.

--- php/iconchecker/IconChecker.php
+++ php/iconchecker/IconChecker.php
@@ -35,6 +35,19 @@ function checkWebLibsIOC($file)
 {
 	echo "CHECK: $file\n";
 	
+	$basename = basename($file);
+	$basename = explode(".", $basename);
+
+	$country = "xx";
+	
+	if (count($basename) == 3)
+	{
+		$country = $basename[ count($basename) - 2 ];
+		$country = substr($country, 0, 2);
+	}
+	
+	$basename = $basename[ 0 ];
+
 	$data = json_decdat(json_decomment(file_get_contents($file)));
 	
 	if ($data == null)
@@ -46,13 +59,15 @@ function checkWebLibsIOC($file)
 	
 	foreach ($data as $catkey => $categories)
 	{
+		$storepath = $basename . "/" . $catkey;
+		
 		if ($catkey == "locales") continue;
 		
 		foreach ($categories as $domainkey => $domain)
 		{
 			if (isset($domain[ "icon" ]))
 			{
-				checkIcon($file, $domainkey, $domain[ "icon" ]);
+				checkIcon($file, $domainkey, $domain[ "icon" ], $storepath);
 			}	
 		}
 	}
@@ -61,6 +76,19 @@ function checkWebLibsIOC($file)
 function checkWebLibsIPxx($file)
 {
 	echo "CHECK: $file\n";
+
+	$basename = basename($file);
+	$basename = explode(".", $basename);
+
+	$country = "xx";
+	
+	if (count($basename) == 3)
+	{
+		$country = $basename[ count($basename) - 2 ];
+		$country = substr($country, 0, 2);
+	}
+	
+	$basename = $basename[ 0 ];
 	
 	$data = json_decdat(json_decomment(file_get_contents($file)));
 	
@@ -73,6 +101,8 @@ function checkWebLibsIPxx($file)
 	
 	foreach ($data as $keytag => $content)
 	{
+		$storepath = $basename;
+
 		if ($keytag == "locales") continue;
 		
 		if (isset($content[ "icon" ]))
@@ -97,11 +127,11 @@ function checkWebLibsIPxx($file)
 	}
 }
 
-function checkIcon($file, $keytag, $icon)
+function checkIcon($file, $keytag, $icon, $storepath = null)
 {
 	$cont = file_get_contents($icon);
 	$result = ($cont == null) ? "FAIL" : "OK";
-	
+
 	if ($result == "FAIL")
 	{
 		$error = array();
@@ -110,9 +140,27 @@ function checkIcon($file, $keytag, $icon)
 		$error[ "icon"   ] = $icon;
 		
 		$GLOBALS[ "fails" ][] = $error;
+
+		echo "CHECK: $result => $keytag => $icon\n";
 	}
-	
-	echo "CHECK: $result => $keytag => $icon\n";
+	else
+	{
+		$info = getimagesizefromstring($cont);
+		$fext = image_type_to_extension($info[ 2 ]);
+		if ($fext == ".jpeg") $fext = ".jpg";
+		
+		echo "CHECK: $result => $keytag => $fext => $icon\n";
+		
+		if ($storepath)
+		{
+			$storefile = "../../asp/SafeHome/app/weblibs/logos/" . $storepath;
+			if (! file_exists($storefile)) mkdir($storefile, 0755, true);
+			$storefile = $storefile . "/" . $keytag . ".img";
+			
+			file_put_contents($storefile, $cont);
+			echo "STORE: $storefile\n";
+		}
+	}
 }
 
 function checkWebLibs()
@@ -133,6 +181,8 @@ function checkWebLibs()
 			checkWebLibsIOC($path . "/" . $entry);
 		}
 
+		continue;
+		
 		if ((substr($entry, 0, 4) == "iptv") ||
 			(substr($entry, 0, 4) == "iprd"))
 		{
